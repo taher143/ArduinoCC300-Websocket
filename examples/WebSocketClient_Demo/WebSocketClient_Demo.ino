19e1de6bed061b37dd1273aeba0fb0c30db06e1c
Taher Kawantwala <taher143@users.noreply.github.com>
2015-01-26 20:26:09 +0530

    Update WebSocketClient_Demo.ino
    Unnecessary comments has been removed and debug serial statements are added

--- examples/WebSocketClient_Demo/WebSocketClient_Demo.ino
+++ examples/WebSocketClient_Demo/WebSocketClient_Demo.ino
@@ -55,23 +55,23 @@ void setup(void)
 {
   Serial.begin(115200);
   Serial.println(F("Hello, CC3000!\n")); 
-/*
+
   Serial.print("Free RAM: "); 
-  Serial.println(getFreeRam(), DEC);*/
+  Serial.println(getFreeRam(), DEC);
 
   /* Initialise the module */
-  //Serial.println(F("\nInitializing..."));
+  Serial.println(F("\nInitializing..."));
   if (!cc3000.begin())
   {
-    //Serial.println(F("Couldn't begin()! Check your wiring?"));
+    Serial.println(F("Couldn't begin()! Check your wiring?"));
     while(1);
   }
 
   // Optional SSID scan
   // listSSIDResults();
 
-  //Serial.print(F("\nAttempting to connect to ")); 
-  //Serial.println(WLAN_SSID);
+  Serial.print(F("\nAttempting to connect to ")); 
+  Serial.println(WLAN_SSID);
   if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
     Serial.println(F("Failed!"));
     while(1);
@@ -80,7 +80,7 @@ void setup(void)
   Serial.println(F("Connected AP!"));
 
   /* Wait for DHCP to complete */
-  //Serial.println(F("Request DHCP"));
+  Serial.println(F("Request DHCP"));
   while (!cc3000.checkDHCP())
   {
     delay(100); // ToDo: Insert a DHCP timeout!
@@ -104,24 +104,9 @@ void setup(void)
 
   cc3000.printIPdotsRev(ip);
 
-  // Optional: Do a ping test on the website
-  /*
-  Serial.print(F("\n\rPinging ")); cc3000.printIPdotsRev(ip); Serial.print("...");  
-   replies = cc3000.ping(ip, 5);
-   Serial.print(replies); Serial.println(F(" replies"));
-   */
-
-  /* Try connecting to the website.
-   Note: HTTP/1.1 protocol is used to keep the server from closing the connection before all data is read.
-   */
   www = cc3000.connectTCP(ip, 80);
   if (www.connected()) {
-    /*    www.fastrprint(F("GET "));
-     www.fastrprint(WEBPAGE);
-     www.fastrprint(F(" HTTP/1.1\r\n"));
-     www.fastrprint(F("Host: ")); www.fastrprint(WEBSITE); www.fastrprint(F("\r\n"));
-     www.fastrprint(F("\r\n"));
-     www.println();*/
+     Serial.println(F("Connection Successfull")); 
   } 
   else {
     Serial.println(F("Connection failed"));    
@@ -130,22 +115,6 @@ void setup(void)
 
   Serial.println(F("-------------------------------------"));
 
-  /* Read data until either the connection is closed, or the idle timeout is reached. */
-  /*  unsigned long lastRead = millis();
-   while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
-   while (www.available()) {
-   char c = www.read();
-   Serial.print(c);
-   lastRead = millis();
-   }
-   }
-   www.close();
-   Serial.println(F("-------------------------------------"));*/
-
-  /* You need to make sure to clean up after yourself or the CC3000 can freak out */
-  /* the next time your try to connect ... */
-  /*  Serial.println(F("\n\nDisconnecting"));
-   cc3000.disconnect();*/
   // Handshake with the server
   webSocketClient.path = WEBPAGE;
   webSocketClient.host = WEBSITE;
@@ -244,7 +213,7 @@ bool displayConnectionDetails(void)
   }
   else
   {
-   /* Serial.print(F("\nIP Addr: ")); 
+   Serial.print(F("\nIP Addr: ")); 
     cc3000.printIPdotsRev(ipAddress);
     Serial.print(F("\nNetmask: ")); 
     cc3000.printIPdotsRev(netmask);
@@ -254,7 +223,7 @@ bool displayConnectionDetails(void)
     cc3000.printIPdotsRev(dhcpserv);
     Serial.print(F("\nDNSserv: ")); 
     cc3000.printIPdotsRev(dnsserv);
-    Serial.println();*/
+    Serial.println();
     return true;
   }
 }
